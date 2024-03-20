0b8d9f53fc4e9e23389e0b6f72c5a94a4261dcc3
SnowArtz <ds.ortiz@uniandes.edu.co>
2024-03-20 11:26:30 -0500

    Buggs Fixed

--- unischedule/lib/routes/go_router_provider.dart
+++ unischedule/lib/routes/go_router_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:unischedule/presentation/calendar_page/calendar_page.dart';
-import 'package:unischedule/presentation/gotty_page/gotty_page.dart';
+import 'package:unischedule/presentation/shell/app_shell.dart';
 import 'package:unischedule/presentation/home_page/home_page.dart';
 
 
@@ -21,40 +21,40 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         path: '/home',
         name: 'root',
         builder: (context, state) {
-          return MyApp(key: state.pageKey);
+          return HomePage(key: state.pageKey);
         }
       ),
 
       ShellRoute(
         navigatorKey: shellNavigator,
-        builder: (context, state, child) => DashboardScreen(key:state.pageKey, child: child),
+        builder: (context, state, child) => AppShell(key:state.pageKey, child: child),
         routes: [
           GoRoute(
             path: '/',
             name: 'home',
             pageBuilder: (context, state) {
-              return NoTransitionPage(child: MyApp(key: state.pageKey));
+              return NoTransitionPage(child: HomePage(key: state.pageKey));
             }
           ),
           GoRoute(
             path: '/calendar',
             name: 'calendar',
             pageBuilder: (context, state) {
-              return NoTransitionPage(child: GottyApp(key: state.pageKey));
+              return NoTransitionPage(child: CalendarApp(key: state.pageKey));
             }
           ),
           GoRoute(
             path: '/friends',
             name: 'friends',
             pageBuilder: (context, state) {
-              return NoTransitionPage(child: GottyApp(key: state.pageKey));
+              return NoTransitionPage(child: HomePage(key: state.pageKey));
             }
           ),
           GoRoute(
             path: '/groups',
             name: 'groups',
             pageBuilder: (context, state) {
-              return NoTransitionPage(child: GottyApp(key: state.pageKey));
+              return NoTransitionPage(child: HomePage(key: state.pageKey));
             }
           )
         ],
