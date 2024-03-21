2acbe5f65f5977dad786afd02e05bb0b9b8f58e1
Sebastian Urrea <sebastian.urrea@caseware.com>
2024-03-21 03:28:06 -0500

    Dropdown and basic info form fields

--- unischedule/lib/presentation/create-class_page/create-class_page.dart
+++ unischedule/lib/presentation/create-class_page/create-class_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:multi_dropdown/multiselect_dropdown.dart';
 
 class CreateClassPage extends StatefulWidget {
   const CreateClassPage({Key? key}) : super(key: key);
@@ -13,10 +14,13 @@ class _CreateClassPageState extends State<CreateClassPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-          children: <Widget>[
-            _buildAppBar(),
-          ]
+      body: Column(
+        children: <Widget>[
+          _buildAppBar(),
+          Expanded(
+            child: _buildCreateClassForm(),
+          ),
+        ]
       ),
       backgroundColor: Color(0xFFF8F8F8),
     );
@@ -34,7 +38,7 @@ class _CreateClassPageState extends State<CreateClassPage> {
           context.go('/calendar'); // FIXME - Pop last route, will need to work on navigation stack
         },
       ),
-      title: Text(
+      title: const Text(
         'Create Class',
         style: TextStyle(
           fontFamily: 'Poppins',
@@ -54,9 +58,283 @@ class _CreateClassPageState extends State<CreateClassPage> {
   }
 
   Widget _buildCreateClassForm() {
-    return Column(
-      children: <Widget>[
-      ],
+
+    const assistants = <String>[
+      'Laura',
+      'Gotty',
+      'Sebastian',
+      'Juan',
+      'Lucciano'
+    ];
+    final MultiSelectController assistantsMultiSelectController = MultiSelectController();
+
+    const reminders = <String>[
+      'No reminder',
+      'At time of event',
+      '5 minutes before',
+      '15 minutes before',
+      '30 minutes before',
+      '1 hour before',
+    ];
+    String? selectedReminder;
+
+    const labels = <String>[
+      'Uniandes 📚',
+      'Work 👩‍💻',
+      'Personal ✨',
+      'Family 👨‍👩‍👧‍👦',
+      'Friends 👯‍',
+    ];
+    final MultiSelectController labelsMultiSelectController = MultiSelectController();
+
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(color: Color(0xFFD0D5DD), width: 1),
+                ),
+                child: const Text(
+                  'One-Time',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    color: Color(0xFF475569),
+                  ),
+                ),
+              ),
+              SizedBox(width: 12),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Color(0xFF9DCC18),
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(color: Color(0xFF9DCC18), width: 1),
+                ),
+                child: const Text(
+                  'Recurrent',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: Color(0xFFD0D5DD), width: 1),
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 56,
+                  child: Row(
+                    children: <Widget>[
+                      const Expanded(
+                        child: TextField(
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            color: Color(0xFF475569),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Event Name',
+                            hintStyle: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              color: Color(0xFF475569),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                        child: SvgPicture.asset('assets/icons/signature.svg',
+                            width: 24, height: 24, color: const Color(0xFF475569)),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xFFD0D5DD),
+                      width: 1,
+                    ),
+                  ),
+                  height: 1,
+                ),
+                Container(
+                  height: 56,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: MultiSelectDropDown(
+                          controller: assistantsMultiSelectController,
+                          onOptionSelected: (List<ValueItem> selectedOptions) {},
+                          options: assistants.map((key) => ValueItem(label: key, value: key)).toList(),
+                          selectionType: SelectionType.multi,
+                          chipConfig: const ChipConfig(wrapType: WrapType.scroll),
+                          optionTextStyle: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            color: Color(0xFF475569),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          borderColor: Colors.transparent,
+                          focusedBorderColor: Colors.transparent,
+                          borderWidth: 0,
+                          focusedBorderWidth: 0,
+                          clearIcon: const Icon(Icons.clear),
+                          padding: const EdgeInsets.all(0),
+                          hint: 'Assistants',
+                          hintStyle: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            color: Color(0xFF475569),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          hintPadding: const EdgeInsets.all(0),
+                        ),
+                      ),
+                      SizedBox(
+                          width: 30,
+                          child: SvgPicture.asset('assets/icons/person-chalkboard.svg',
+                              width: 24, height: 24, color: const Color(0xFF475569)),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xFFD0D5DD),
+                      width: 1,
+                    ),
+                  ),
+                  height: 1,
+                ),
+                Container(
+                  height: 56,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Reminder',
+                            hintStyle: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16,
+                              color: Color(0xFF475569),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          items: reminders.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: const TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 16,
+                                  color: Color(0xFF475569),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedReminder = newValue;
+                            });
+                          },
+                          value: selectedReminder,
+                        ),
+                      ),
+                      SizedBox(
+                          width: 30,
+                          child: SvgPicture.asset('assets/icons/stopwatch.svg',
+                              width: 24, height: 24, color: Color(0xFF475569)),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xFFD0D5DD),
+                      width: 1,
+                    ),
+                  ),
+                  height: 1,
+                ),
+                Container(
+                  height: 56,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: MultiSelectDropDown(
+                          controller: labelsMultiSelectController,
+                          onOptionSelected: (List<ValueItem> selectedOptions) {},
+                          options: labels.map((key) => ValueItem(label: key, value: key)).toList(),
+                          selectionType: SelectionType.multi,
+                          chipConfig: const ChipConfig(wrapType: WrapType.scroll),
+                          optionTextStyle: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            color: Color(0xFF475569),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          borderColor: Colors.transparent,
+                          focusedBorderColor: Colors.transparent,
+                          borderWidth: 0,
+                          focusedBorderWidth: 0,
+                          clearIcon: Icon(Icons.clear),
+                          padding: EdgeInsets.all(0),
+                          hint: 'Assistants',
+                          hintStyle: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            color: Color(0xFF475569),
+                            fontWeight: FontWeight.bold,
+                          ),
+                          hintPadding: const EdgeInsets.all(0),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                        child: SvgPicture.asset('assets/icons/tag.svg',
+                            width: 24, height: 24, color: const Color(0xFF475569)),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Card(),
+          Row(),
+          Row(),
+          Card(),
+        ],
+      ),
     );
   }
 
