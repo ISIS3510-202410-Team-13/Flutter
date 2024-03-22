adb2a04cb84ef89010a02961106a69513aa4346f
SnowArtz <ds.ortiz@uniandes.edu.co>
2024-03-22 10:46:54 -0500

    Pop up for rating recommendation finished

--- unischedule/lib/presentation/create-class_page/create-class_page.dart
+++ unischedule/lib/presentation/create-class_page/create-class_page.dart
@@ -13,6 +13,8 @@ import 'package:unischedule/services/notifications_service.dart';
 import '../../../providers/create-class_page/create-class_provider.dart';
 import 'package:intl/intl.dart';
 
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 
 class CreateClassPage extends ConsumerStatefulWidget {
   const CreateClassPage({Key? key}) : super(key: key);
@@ -683,38 +685,142 @@ class PlaceRecommendationsDialog extends ConsumerWidget {
                 const SizedBox(width: 48),
                 TextButton(
                   onPressed: () {
-                    Navigator.of(context).pop(); // Cierra el pop-up actual
+                    Navigator.of(context).pop(); // Cierra el diálogo de recomendaciones de lugares actual
 
-                    // Muestra el nuevo pop-up desde abajo
+                    // Muestra el nuevo ModalBottomSheet desde abajo
                     showModalBottomSheet(
                       context: context,
-                      builder: (BuildContext context) {
-                        return BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                          child: Container(
-                            height: MediaQuery.of(context).size.height * 0.5, // Ocupa media pantalla
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20),
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Hola mundo',
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
+                        builder: (BuildContext context) {
+                          return BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * 0.3, // Cambiar el valor para ajustar el tamaño del modal
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20),
                                 ),
                               ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Text(
+                                      'Rate your recommendations', // Agregar el texto deseado
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 28,
+                                        color: Colors.black,
+                                        decoration: TextDecoration.none,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Center(
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width * 1, // Cambiar el valor para ajustar el ancho del Rating Bar
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            RatingBar.builder(
+                                              initialRating: 5,
+                                              minRating: 1,
+                                              direction: Axis.horizontal,
+                                              allowHalfRating: true,
+                                              itemPadding: EdgeInsets.symmetric(horizontal: 6.0),
+                                              itemBuilder: (context, index) {
+                                                switch(index){
+                                                  case 0:
+                                                    return Icon(
+                                                      Icons.sentiment_very_dissatisfied,
+                                                      color: Colors.red,
+                                                    );
+                                                  case 1:
+                                                    return Icon(
+                                                      Icons.sentiment_dissatisfied,
+                                                      color: Colors.redAccent,
+                                                    );
+                                                  case 2:
+                                                    return Icon(
+                                                      Icons.sentiment_neutral,
+                                                      color: Colors.amber,
+                                                    );
+                                                  case 3:
+                                                    return Icon(
+                                                      Icons.sentiment_satisfied,
+                                                      color: Colors.lightGreen,
+                                                    );
+                                                  case 4:
+                                                    return Icon(
+                                                      Icons.sentiment_very_satisfied,
+                                                      color: Colors.green,
+                                                    );
+                                                  default:
+                                                    return Container();
+                                                }
+                                              },
+                                              onRatingUpdate: (rating) {
+                                                print(rating);
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 0), // Espacio entre el Rating Bar y los botones
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text(
+                                          'Cancel', // Agregar el texto deseado
+                                          style: TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 20,
+                                            color: Colors.red, // Cambiar el color deseado
+                                            decoration: TextDecoration.none,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 32), // Espacio entre los botones
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          // Lógica para el botón "Submit"
+                                          Navigator.of(context).pop();
+                                        },
+                                        style: ButtonStyle(
+                                          backgroundColor: MaterialStateProperty.all(Colors.green), // Cambiar el color deseado
+                                          shape: MaterialStateProperty.all(
+                                            RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(8),
+                                            ),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          'Submit', // Agregar el texto deseado
+                                          style: TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 20,
+                                            color: Colors.white,
+                                            decoration: TextDecoration.none,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      isScrollControlled: true, // Permite que el sheet ocupe más espacio
-                      backgroundColor: Colors.transparent, // Hace el fondo transparente para permitir el efecto blur
-                    );
+                          );
+                        },
+                        isScrollControlled: true, // Permite que el sheet ocupe más espacio
+                        backgroundColor: Colors.transparent, // Hace el fondo transparente para permitir el efecto blur
+                      );
                   },
                   child: const Text(
                     'Confirm',
@@ -726,6 +832,8 @@ class PlaceRecommendationsDialog extends ConsumerWidget {
                     ),
                   ),
                 )
+
+
 
               ],
             )
