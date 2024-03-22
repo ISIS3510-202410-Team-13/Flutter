b1577b07b7e5c2d0296a51df04962fc105eea1ad
Sebastian Urrea <sebastian.urrea@caseware.com>
2024-03-22 00:04:15 -0500

    Repository

---
+++ unischedule/lib/repositories/create-class_page/create-class_repository.dart
@@ -0,0 +1,26 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../../../models/create-class_page/create-class_model.dart';
+import '../../../services/network/available-spaces_api_service.dart';
+
+part 'create-class_repository.g.dart';
+
+abstract class AvailableSpacesRepository {
+  Future<List<AvailableSpacesResponseModel>> fetchAvailableSpaces(AvailableSpacesRequestModel request);
+}
+
+class AvailableSpacesRepositoryImpl extends AvailableSpacesRepository {
+  final AvailableSpacesApiService client;
+
+  AvailableSpacesRepositoryImpl({required this.client});
+
+  @override
+  Future<List<AvailableSpacesResponseModel>> fetchAvailableSpaces(AvailableSpacesRequestModel request) async {
+    var response = await client.getRequest('', request.toJson());
+    return response as List<AvailableSpacesResponseModel>;
+  }
+}
+
+@riverpod
+AvailableSpacesRepositoryImpl availableSpacesRepository(AvailableSpacesRepositoryRef ref) {
+  return AvailableSpacesRepositoryImpl(client: AvailableSpacesApiService());
+}
