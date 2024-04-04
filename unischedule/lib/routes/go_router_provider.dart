b5a8a82317f6449b4b487744d047792ea5182bc5
SnowArtz <ds.ortiz@uniandes.edu.co>
2024-04-04 03:05:39 -0500

    Remove Initial Notifications Page, Fix Bug Router Provider

--- unischedule/lib/routes/go_router_provider.dart
+++ unischedule/lib/routes/go_router_provider.dart
@@ -3,19 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:unischedule/presentation/calendar_page/calendar_page.dart';
 import 'package:unischedule/presentation/create-class_page/create-class_page.dart';
-import 'package:unischedule/presentation/notifications_page/notifications_page.dart';
-import 'package:unischedule/presentation/rating-bar_page/rating-bar_page.dart';
 import 'package:unischedule/presentation/shell/app_shell.dart';
 import 'package:unischedule/presentation/home_page/home_page.dart';
 import 'package:unischedule/presentation/authentication_page/authentication_page.dart';
 import 'package:unischedule/presentation/friends_page/friends_page.dart';
 import 'package:unischedule/presentation/groups_page/groups_page.dart';
-
-
-
-
-
-
 
 final GlobalKey<NavigatorState> rootNavigator = GlobalKey(debugLabel: 'root');
 final GlobalKey<NavigatorState> shellNavigator = GlobalKey(debugLabel: 'shell');
@@ -32,47 +24,41 @@ final goRouterProvider = Provider<GoRouter>((ref) {
           return AuthenticationPage(key: state.pageKey);
         },
       ),
-
       GoRoute(
-        path: '/create-class',
-        name: 'create-class',
-        builder: (context, state) {
-          return CreateClassPage(key: state.pageKey);
-        }
-      ),
-
+          path: '/create-class',
+          name: 'create-class',
+          builder: (context, state) {
+            return CreateClassPage(key: state.pageKey);
+          }),
       ShellRoute(
         navigatorKey: shellNavigator,
-        builder: (context, state, child) => AppShell(key:state.pageKey, child: child),
+        builder: (context, state, child) =>
+            AppShell(key: state.pageKey, child: child),
         routes: [
           GoRoute(
-            path: '/home',
-            name: 'home',
-            pageBuilder: (context, state) {
-              return NoTransitionPage(child: HomePage(key: state.pageKey));
-            }
-          ),
+              path: '/home',
+              name: 'home',
+              pageBuilder: (context, state) {
+                return NoTransitionPage(child: HomePage(key: state.pageKey));
+              }),
           GoRoute(
-            path: '/calendar',
-            name: 'calendar',
-            pageBuilder: (context, state) {
-              return NoTransitionPage(child: CalendarApp(key: state.pageKey));
-            }
-          ),
+              path: '/calendar',
+              name: 'calendar',
+              pageBuilder: (context, state) {
+                return NoTransitionPage(child: CalendarApp(key: state.pageKey));
+              }),
           GoRoute(
-            path: '/friends',
-            name: 'friends',
-            pageBuilder: (context, state) {
-              return NoTransitionPage(child: FriendsApp(key: state.pageKey));
-            }
-          ),
+              path: '/friends',
+              name: 'friends',
+              pageBuilder: (context, state) {
+                return NoTransitionPage(child: FriendsApp(key: state.pageKey));
+              }),
           GoRoute(
-            path: '/groups',
-            name: 'groups',
-            pageBuilder: (context, state) {
-              return NoTransitionPage(child: GroupsPage(key: state.pageKey));
-            }
-          )
+              path: '/groups',
+              name: 'groups',
+              pageBuilder: (context, state) {
+                return NoTransitionPage(child: GroupsPage(key: state.pageKey));
+              })
         ],
       )
     ],
