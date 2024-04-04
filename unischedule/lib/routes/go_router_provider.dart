a8c373fa88423ed3763cab2982fae585bdfd1197
Gotty <78111224+iGotty@users.noreply.github.com>
2024-04-03 23:52:29 -0500

    Implemented Friends View
    Implemented a Flutter view with custom AppBar and ListView for friends, featuring search bar and a FloatingActionButton for new actions.

--- unischedule/lib/routes/go_router_provider.dart
+++ unischedule/lib/routes/go_router_provider.dart
@@ -3,12 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:unischedule/presentation/calendar_page/calendar_page.dart';
 import 'package:unischedule/presentation/create-class_page/create-class_page.dart';
-import 'package:unischedule/presentation/groups_page/groups_page.dart';
 import 'package:unischedule/presentation/notifications_page/notifications_page.dart';
 import 'package:unischedule/presentation/rating-bar_page/rating-bar_page.dart';
 import 'package:unischedule/presentation/shell/app_shell.dart';
 import 'package:unischedule/presentation/home_page/home_page.dart';
 import 'package:unischedule/presentation/authentication_page/authentication_page.dart';
+import 'package:unischedule/presentation/friends_page/friends_page.dart';
+import 'package:unischedule/presentation/groups_page/groups_page.dart';
 
 
 
@@ -62,7 +63,7 @@ final goRouterProvider = Provider<GoRouter>((ref) {
             path: '/friends',
             name: 'friends',
             pageBuilder: (context, state) {
-              return NoTransitionPage(child: RatingPage(key: state.pageKey, title: "Snow"));
+              return NoTransitionPage(child: FriendsApp(key: state.pageKey));
             }
           ),
           GoRoute(
