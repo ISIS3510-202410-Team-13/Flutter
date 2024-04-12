927ae485f9b4c10b9eda69ff5a70022d6279e5fd
Gotty <78111224+iGotty@users.noreply.github.com>
2024-04-11 22:23:41 -0500

    Implement local filtering for Friends and Groups using StateNotifier
    This commit introduces a local filtering functionality for both the Friends and Groups sections of the application. StateNotifiers have been added to manage the state of friends and groups lists effectively. The initial data loading is now handled in the AuthenticationPage, ensuring all data is loaded once and managed through StateNotifiers for optimal performance and responsiveness.
    
    Changes:
    - Added FriendsStateNotifier and GroupsStateNotifier for managing friends and groups lists respectively.
    - Modified AuthenticationPage to initialize StateNotifiers with the loaded data.
    - Updated GroupsPage and FriendsPage to use the StateNotifiers for local filtering based on user input.
    - Ensured that data loading is only executed once to prevent unnecessary API calls and data reloading.

--- unischedule/lib/presentation/groups_page/groups_page.dart
+++ unischedule/lib/presentation/groups_page/groups_page.dart
@@ -3,8 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:math';
 import 'package:go_router/go_router.dart';
-import '../../../providers/groups_page/groups_provider.dart'; // Asegúrate de que este sea el proveedor correcto
-import '../../../models/groups_page/group_model.dart'; // Importa el modelo de grupo
+import '../../../providers/groups_page/groups_provider.dart';
+import '../../../models/groups_page/group_model.dart';
+import '../../../providers/groups_page/groups_state_notifier.dart';
 
 class GroupsPage extends ConsumerStatefulWidget {
   const GroupsPage({Key? key}) : super(key: key);
@@ -16,182 +17,186 @@ class GroupsPage extends ConsumerStatefulWidget {
 class _GroupsPageState extends ConsumerState<GroupsPage> {
   final TextEditingController _searchController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    // Observa el proveedor de grupos con el ID del usuario especificado
-    final groupsAsyncValue = ref.watch(groupsProvider('0MebgXs8fBYREjDKMlwq'));
+@override
+Widget build(BuildContext context) {
+  final groups = ref.watch(groupsStateNotifierProvider);
+  return Scaffold(
+    appBar: _buildAppBar(context),
+    body: Column(
+      children: [
+        _buildSearchBar(),
+        _buildGroupsList(groups),
+      ],
+    ),
+    floatingActionButton: _buildFloatingActionButton(),
+  );
+}
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset(
-            'assets/icons/bars.svg',
-            width: 21,
-            height: 24,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            context.pop();
-          },
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          'assets/icons/bars.svg',
+          width: 21,
+          height: 24,
+          color: Colors.black,
         ),
-        title: const Text('Groups',
-            style: TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black)),
+        onPressed: () => context.pop(),
       ),
-      body: Column(
-        children: [
-          // Barra de búsqueda, si es necesaria
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-            child: Container(
-              height: 45,
-              decoration: BoxDecoration(
-                color: Color(0xFFD9D9D9),
-                borderRadius: BorderRadius.circular(16),
+      title: const Text('Groups',
+          style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black)),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: Container(
+        height: 45,
+        decoration: BoxDecoration(
+          color: Color(0xFFD9D9D9),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: TextField(
+          controller: _searchController,
+          onChanged: (value) {
+          ref.read(groupsStateNotifierProvider.notifier).filterGroups(value);
+        },
+          style: const TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Color(0xFF686868),
+          ),
+          decoration: InputDecoration(
+            hintText: 'Search',
+            hintStyle: const TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFF686868),
+            ),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: SvgPicture.asset(
+                'assets/icons/magnifying-glass.svg',
+                width: 24,
+                height: 24,
+                color: Color(0xFF686868),
               ),
-              child: TextField(
-                controller: _searchController,
-                style: TextStyle(
+            ),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+Widget _buildGroupsList(List<Group> groups) {
+  return Expanded(
+    child: ListView.builder(
+      itemCount: groups.length,
+      itemBuilder: (context, index) => _buildGroupItem(groups[index]),
+    ),
+  );
+}
+
+  Widget _buildGroupItem(Group group) {
+    final bgColor = Color(int.parse(group.color.replaceAll('#', '0xff')));
+    final colorMasOscuro = bgColor
+        .withRed(max(0, bgColor.red - 50))
+        .withGreen(max(0, bgColor.green - 50))
+        .withBlue(max(0, bgColor.blue - 50));
+
+    return Container(
+      width: double.infinity,
+      height: 133,
+      margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, bottom: 10),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                group.name.length > 15
+                    ? '${group.name.substring(0, 15)}...'
+                    : group.name,
+                style: const TextStyle(
                   fontFamily: 'Poppins',
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xFF686868),
-                ),
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  hintStyle: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xFF686868),
-                  ),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: SvgPicture.asset(
-                      'assets/icons/magnifying-glass.svg',
-                      width: 24,
-                      height: 24,
-                      color: Color(0xFF686868),
-                    ),
-                  ),
-                  border: InputBorder.none,
+                  fontSize: 30,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
                 ),
               ),
             ),
           ),
-          Expanded(
-            child: groupsAsyncValue.when(
-              data: (groups) => ListView.builder(
-                itemCount: groups.length,
-                itemBuilder: (context, index) {
-                  final Group group = groups[index];
-                  final bgColor =
-                      Color(int.parse(group.color.replaceAll('#', '0xff')));
-                  Color colorMasOscuro = bgColor
-                      .withRed(max(0, bgColor.red - 50))
-                      .withGreen(max(0, bgColor.green - 50))
-                      .withBlue(max(0, bgColor.blue - 50));
-
-                  return Container(
-                    width: double.infinity,
-                    height: 133,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                    decoration: BoxDecoration(
-                      color: bgColor,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20, bottom: 10),
-                          child: Align(
-                            alignment: Alignment.bottomLeft,
-                            child: Text(
-                              group.name.length > 15
-                                  ? '${group.name.substring(0, 15)}...'
-                                  : group.name,
-                              style: const TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 30,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          right: 0,
-                          top: 0,
-                          child: CustomPaint(
-                            size: const Size(100, 100),
-                            painter: MyCustomPainter(colorMasOscuro),
-                          ),
-                        ),
-                        Positioned(
-                          right: 10,
-                          top: 10,
-                          child: Column(
-                            children: const [
-                              CircleAvatar(
-                                  radius: 3, backgroundColor: Colors.white),
-                              SizedBox(height: 3),
-                              CircleAvatar(
-                                  radius: 3, backgroundColor: Colors.white),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          left: 10,
-                          top: 12,
-                          child: ProfileIconsRow(
-                              memberCount: group.members.length, imagePaths:group.profilePictures),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              loading: () => const Center(child: CircularProgressIndicator()),
-              error: (error, stack) => Center(child: Text('Error: $error')),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: CustomPaint(
+              size: const Size(100, 100),
+              painter: MyCustomPainter(colorMasOscuro),
             ),
+          ),
+          Positioned(
+            right: 10,
+            top: 10,
+            child: Column(
+              children: const [
+                CircleAvatar(radius: 3, backgroundColor: Colors.white),
+                SizedBox(height: 3),
+                CircleAvatar(radius: 3, backgroundColor: Colors.white),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 10,
+            top: 12,
+            child: ProfileIconsRow(
+                memberCount: group.members.length,
+                imagePaths: group.profilePictures),
           ),
         ],
       ),
-      floatingActionButton: Container(
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF9DCC18), // Color de fondo verde
-              shape: BoxShape
-                  .circle, // Forma circular para que coincida con el FAB
-            ),
-            padding: EdgeInsets.all(
-                0), // Espacio alrededor del icono para reducir el tamaño del fondo verde
-            child: SvgPicture.asset(
-              'assets/icons/square-plus.svg',
-              width: 50, // Ancho del icono reducido
-              height: 56, // Altura del icono reducida
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.transparent,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10), // Bordes redondeados
-            side: BorderSide(
-              color: Color(0xFF9DCC18), // Color del borde
-              width: 3.8, // Ancho del borde
-            ),
-          ),
-          elevation: 0,
+    );
+  }
+
+  Widget _buildFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF9DCC18),
+          shape: BoxShape.circle,
+        ),
+        padding: const EdgeInsets.all(0),
+        child: SvgPicture.asset(
+          'assets/icons/square-plus.svg',
+          width: 50,
+          height: 56,
+          color: Colors.white,
         ),
       ),
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: BorderSide(
+          color: Color(0xFF9DCC18),
+          width: 3.8,
+        ),
+      ),
+      elevation: 0,
     );
   }
 }
---
+++ unischedule/lib/providers/friends_page/friends_state_notifier.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:unischedule/models/friends_page/friend_model.dart';
+
+class FriendsStateNotifier extends StateNotifier<List<Friend>> {
+  List<Friend> allFriends = [];
+
+  FriendsStateNotifier(List<Friend> initialFriends) : super(initialFriends) {
+    allFriends = initialFriends;
+  }
+
+  void filterFriends(String searchText) {
+    if (searchText.isEmpty) {
+      state = allFriends;
+    } else {
+      state = allFriends.where((friend) =>
+        friend.name.toLowerCase().contains(searchText.toLowerCase())).toList();
+    }
+  }
+}
+
+final friendsStateNotifierProvider = StateNotifierProvider<FriendsStateNotifier, List<Friend>>((ref) {
+  return FriendsStateNotifier([]);
+});
