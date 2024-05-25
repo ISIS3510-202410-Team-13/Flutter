0b87425448b490c9cb70f4a080fec6e465b52995
Gotty <78111224+iGotty@users.noreply.github.com>
2024-05-25 01:03:26 -0500

    LocalStorage for notifications and respective tab views

--- unischedule/lib/views/notifications/widgets/notifications_app_bar.dart
+++ unischedule/lib/views/notifications/widgets/notifications_app_bar.dart
@@ -1,48 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:unischedule/constants/constants.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:unischedule/providers/providers.dart';
+import 'package:unischedule/models/notifications/notification_model.dart';
 
-class NotificationsAppBar extends ConsumerStatefulWidget implements PreferredSizeWidget {
-  const NotificationsAppBar({super.key});
+class UniScheduleAppBar extends ConsumerWidget implements PreferredSizeWidget {
+  const UniScheduleAppBar({
+    super.key,
+    required this.color,
+    required this.title,
+  });
 
-  @override
-  ConsumerState<NotificationsAppBar> createState() => _NotificationsAppBarState();
+  final Color color;
+  final String title;
 
   @override
   Size get preferredSize => const Size.fromHeight(StyleConstants.appBarHeight);
-}
-
-class _NotificationsAppBarState extends ConsumerState<NotificationsAppBar> {
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final connectivityStatus = ref.watch(connectivityStatusProvider);
+    final allNotificationsFuture = ref.watch(fetchNotificationsProvider);
+
     return AppBar(
       backgroundColor: ColorConstants.transparent,
       elevation: 0,
-      centerTitle: true,
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium?.copyWith(
+          color: color,
+        ),
+      ),
       leading: IconButton(
         icon: SvgPicture.asset(
-            'assets/icons/arrow-left.svg', // TODO use AssetConstants
-            width: 21, // TODO use StyleConstants
-            height: 24,  // TODO use StyleConstants
-            color: ColorConstants.black
+          AssetConstants.icBars,
+          width: StyleConstants.iconWidth,
+          height: StyleConstants.iconHeight,
+          color: color,
         ),
         onPressed: () {
-          context.pop();
+          Scaffold.of(context).openDrawer();
         },
       ),
-      title: const Text(
-        'Notifications', // TODO use StringConstants
-        style: TextStyle( // TODO use text theme
-          fontFamily: 'Poppins',
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.black,
+      actions: <Widget>[
+        // Add Non-clickable Connectivity Icon
+        if (connectivityStatus == ConnectivityStatus.isDisconnected)
+          Icon(Icons.wifi_off, color: color, size: 28)
+        else
+          Icon(Icons.wifi, color: color, size: 28),
+        // Existing Notification Button
+        allNotificationsFuture.when(
+          data: (notifications) {
+            final unreadCount = notifications.where((n) => !n.viewed).length;
+            return IconButton(
+              icon: Icon(
+                unreadCount >= 1 ? Icons.notifications_active : Icons.notifications,
+                color: color,
+                size: 28,
+              ),
+              onPressed: () {
+                context.push(RouteConstants.notifications);
+              },
+            );
+          },
+          loading: () => Icon(Icons.notifications, color: color, size: 28),
+          error: (err, stack) => Icon(Icons.error, color: color, size: 28),
         ),
-        textAlign: TextAlign.start,
-      ),
+      ],
     );
   }
 }
