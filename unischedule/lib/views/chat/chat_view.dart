70a718392074f02ecf7a99c8b94ecab41cdaabcf
Sebastian Urrea <sebastian.urrea@caseware.com>
2024-05-25 20:52:33 -0500

    Chat App Bar

--- unischedule/lib/views/chat/chat_view.dart
+++ unischedule/lib/views/chat/chat_view.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:unischedule/constants/colors/color_constants.dart';
+import 'package:unischedule/providers/providers.dart';
+import 'widgets/chat_top_bar.dart';
 
-class ChatView extends StatelessWidget {
-  const ChatView({super.key});
+class ChatView extends ConsumerStatefulWidget {
+  final String otherUserId;
+
+  const ChatView({
+    super.key,
+    required this.otherUserId,
+  });
 
   @override
+  ConsumerState<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends ConsumerState<ChatView> {
+  @override
   Widget build(BuildContext context) {
-    // TODO implement this view
-    return const Center(child: Text('Chat works! Use back button :)'));
+
+    final user = ref.watch(authenticationStatusProvider);
+    final otherUser = ref.watch(getFriendProvider(friendId: widget.otherUserId));
+
+    if (user == null || otherUser == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: ColorConstants.white,
+      appBar: ChatTopBar(
+        otherUserName: otherUser.name,
+        otherUserPhotoUrl: otherUser.profilePicture,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
   }
 }
